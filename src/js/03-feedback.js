95960adbb44a3efe85c377ba4f0c2a68b1a46f40
Voytesha <elflowers.info@gmail.com>
2023-02-02 00:57:50 +0200

    third done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
 
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name="email"]'),
+    message: document.querySelector('[name="message"]'),
+};
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || { email: '', message: '' };
+console.log(formData);
+
+
+refs.form.addEventListener('input', throttle(onDataInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+savedDataform();
+
+function onDataInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(formData);
+}
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.clear;
+    evt.target.reset();
+    console.log('Storage:', JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  formData = { email: '', message: '' };
+  localStorage.removeItem(STORAGE_KEY);
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || { email: '', message: '' };
+}
+
+
+function savedDataform() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if(savedData) {
+        refs.email.value = savedData.email;
+        refs.message.value = savedData.message;
+    }
+}
+
+refs.email.setAttribute('required', "");
+refs.message.setAttribute('required', "");

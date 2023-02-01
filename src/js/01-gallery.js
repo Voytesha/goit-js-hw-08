734b63f5c9bcbcff7201749f5991ccf45e15994f
Voytesha <elflowers.info@gmail.com>
2023-02-01 21:07:11 +0200

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImagesMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description})=>{
+        return `<a class='gallery__item' href='${original}'>
+        <img  class='gallery__image' src ='${preview}' data-source='${original}' alt ='${description}'>
+        </a>`;}).join('');
+}
+
+galleryList.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
